d9f97d7452eab1ade8f4aad463773d7d8f9cb8bd
HipsDestroyerFTW <87593216+HipsDestroyerFTW@users.noreply.github.com>
2022-04-04 15:09:11 -0400

    Update docker.py

--- docker.py
+++ docker.py
@@ -6,7 +6,7 @@ os.system("apt-get install \
     ca-certificates \
     curl \
     gnupg \
-    lsb-release"
+    lsb-release")
 os.system("apt update")
 os.system("curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg")
 os.system("apt-get update")
