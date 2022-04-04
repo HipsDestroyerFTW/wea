679d7726128e2206db434714c28f91840c426453
HipsDestroyerFTW <87593216+HipsDestroyerFTW@users.noreply.github.com>
2022-04-04 14:21:14 -0400

    Create docker.py
    instalacion de docker automatica (DEBE USAR SUDO SU PRIMERO)

---
+++ docker.py
@@ -0,0 +1,17 @@
+import os
+import subprocess
+
+os.system("apt update")
+os.system("apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release"
+os.system("apt update")
+os.system("curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg")
+os.system("apt-get update")
+os.system("apt-get install docker-ce docker-ce-cli containerd.io")
+os.system("echo Gracias por utilizar este script! cualquier consulta, dirigirse a https://github.com/HipsDestroyerFTW/")
+os.system("exit")
+
+//
