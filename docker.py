9712d64ad8cceb43bfaa1e284616fa8c412cbfb7
HipsDestroyerFTW <87593216+HipsDestroyerFTW@users.noreply.github.com>
2022-04-04 15:11:43 -0400

    Update docker.py

--- docker.py
+++ docker.py
@@ -10,7 +10,7 @@ os.system("apt-get install \
 os.system("apt update")
 os.system("curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg")
 os.system("apt-get update")
-os.system("apt-get install docker-ce docker-ce-cli containerd.io")
+os.system("apt-get install docker-ce docker-ce-cli containerd.io -y")
 os.system("echo Gracias por utilizar este script! cualquier consulta, dirigirse a https://github.com/HipsDestroyerFTW/")
 os.system("exit")
 
