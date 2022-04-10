335dcc8580b31585336f9b08a2efdfa284b4f97f
HipsDestroyerFTW <87593216+HipsDestroyerFTW@users.noreply.github.com>
2022-04-10 01:08:59 -0400

    Update caddy_install.py

--- caddy_install.py
+++ caddy_install.py
@@ -2,7 +2,7 @@ import os
 
 os.system('apt update')
 os.system('apt install -y debian-keyring debian-archive-keyring apt-transport-https')
-os.system('curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key')
+os.system('curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key'')
 os.system('sudo tee /etc/apt/trusted.gpg.d/caddy-stable.asc')
 os.system('curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list')
 os.system('apt update')
