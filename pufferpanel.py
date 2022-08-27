c1e6947a4019eddedda4c8a65b607b78cd71d198
HipsDestroyerFTW <87593216+HipsDestroyerFTW@users.noreply.github.com>
2022-08-27 02:28:48 -0400

    Create pufferpanel.py

---
+++ pufferpanel.py
@@ -0,0 +1,19 @@
+import os
+
+##install of dependencies
+os.system('sudo apt install software-properties-common') 
+os.system('sudo apt-add-repository universe')
+os.system('sudo apt install -y openssl curl nginx mysql-client mysql-server php-fpm php-cli php-curl php-mysql')
+
+## making sure we have a sudo password for mysql
+os.system('sudo mysql_secure_installation')
+
+## Download of git, curl and pufferpanel, also the install itself
+os.system('sudo mkdir -p /srv && cd /srv')
+os.system('sudo apt install git')
+os.system('sudo apt install curl')
+os.system('curl -L -o pufferpanel.tar.gz https://git.io/fNZYg')
+os.system('tar -xf pufferpanel.tar.gz')
+os.system('cd pufferpanel')
+os.system('chmod +x pufferpanel')
+os.system('sudo ./pufferpanel install')
